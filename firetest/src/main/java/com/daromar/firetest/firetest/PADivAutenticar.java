eda1e9a7e24394f6b4b06d664935e2206b95bf29
daniel.rodriguez <daniel.rodriguez@GSWK09.mshome.net>
2017-12-28 13:06:15 +0100

    Crear DIV

---
+++ firetest/src/main/java/com/daromar/firetest/firetest/PADivAutenticar.java
@@ -0,0 +1,59 @@
+package com.daromar.firetest.firetest;
+
+import com.daromar.firebase.FireButton;
+import com.daromar.firebase.FireDiv;
+import com.daromar.firebase.FireLabel;
+import com.daromar.firebase.FireTextBox;
+import com.daromar.firebase.IButtonClick;
+
+public class PADivAutenticar
+extends FireDiv {
+	
+	private FireButton btnAceptar;
+	private FireTextBox txtUsuario;
+	private FireTextBox txtContrasena;
+	private FireLabel lblError;
+	
+	public PADivAutenticar(String id) {
+		super(id);
+		
+		btnAceptar=new FireButton("btnAceptar");
+		btnAceptar.setCaption("Aceptar2");
+		this.AddControl(btnAceptar);
+		
+		txtUsuario=new FireTextBox("txtUsuario");
+		txtUsuario.setLabel("User");
+		this.AddControl(txtUsuario);
+		
+		
+		txtContrasena=new FireTextBox("txtContrasena");
+		txtContrasena.setLabel("Password");
+		this.AddControl(txtContrasena);
+		
+		lblError=new FireLabel("lblError");
+		this.AddControl(lblError);
+		
+		PADivAutenticar div=this;
+		
+		btnAceptar.AddClickListener(new IButtonClick() {
+
+			@Override
+			public void Click() {
+				String user=txtUsuario.getValue();
+				String pass=txtContrasena.getValue();
+				
+				if (user.compareTo("manager")==0 && pass.compareTo("secure")==0) {
+					lblError.setValue("Ok");
+					PrimeraApplicacion pa=(PrimeraApplicacion)div.getApp();
+					pa.AutenticacionCorrecta();
+					
+					/*divMenu.setDisplay("block");*/
+				}else {
+					lblError.setValue("Usuario o contraseña incorrectarrrr");
+				}
+			}
+			
+		});
+	}
+
+}
--- firetest/src/main/java/com/daromar/firetest/firetest/PrimeraApplicacion.java
+++ firetest/src/main/java/com/daromar/firetest/firetest/PrimeraApplicacion.java
@@ -10,67 +10,28 @@ import com.daromar.firebase.IButtonClick;
 public class PrimeraApplicacion extends FireAppSQLLite {
 	
 
-	private FireDiv divAutenticar;
+	private PADivAutenticar divAutenticar;
 	private FireDiv divMenu;
-	
-	private FireButton btnAceptar;
-	private FireTextBox txtUsuario;
-	private FireTextBox txtContrasena;
-	private FireLabel lblError;
+
 	
 	
 	public PrimeraApplicacion(String basePath,String sqllite) {
 		super(basePath,sqllite);
 		
-		divAutenticar=new FireDiv("divAutenticar");
+		divAutenticar=new PADivAutenticar("divAutenticar");
 		this.AddControl(divAutenticar);
 		
-		
-		btnAceptar=new FireButton("btnAceptar");
-		btnAceptar.setCaption("Aceptar");
-		divAutenticar.AddControl(btnAceptar);
-		
-		txtUsuario=new FireTextBox("txtUsuario");
-		txtUsuario.setLabel("User");
-		divAutenticar.AddControl(txtUsuario);
-		
-		
-		txtContrasena=new FireTextBox("txtContrasena");
-		txtContrasena.setLabel("Password");
-		divAutenticar.AddControl(txtContrasena);
-		
-		lblError=new FireLabel("lblError");
-		divAutenticar.AddControl(lblError);
 		
 		divMenu=new FireDiv("divMenu");
 		divMenu.setDisplay("none");
 		this.AddControl(divMenu);
 		
 		this.InitializeApp();
-		
-
-				
-		
-		btnAceptar.AddClickListener(new IButtonClick() {
-
-			@Override
-			public void Click() {
-				String user=txtUsuario.getValue();
-				String pass=txtContrasena.getValue();
-				
-				if (user.compareTo("manager")==0 && pass.compareTo("secure")==0) {
-					lblError.setValue("Ok");
-					divAutenticar.setDisplay("none");
-					divMenu.setDisplay("block");
-				}else {
-					lblError.setValue("Usuario o contraseña incorrectarrrr");
-				}
-			}
-			
-		});
-		
-
-		
+	}
+	
+	public void AutenticacionCorrecta() {
+		divAutenticar.setDisplay("none");
+		divMenu.setDisplay("block");
 	}
 	
 	
